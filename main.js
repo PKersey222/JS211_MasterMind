6ef595fe97b2bfaf2f8363d98bcc230e42838e84
Philip-Kersey <8fullhouse8@gmail.com>
2022-07-11 12:31:29 -0500

    mastermind testing complete

--- main.js
+++ main.js
@@ -1,77 +1,116 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let board = [];
-let solution = '';
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let solution = "";
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-const printBoard = () =>  {
+const printBoard = () => {
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
-}
+};
 
-const generateSolution = () =>  {
+const generateSolution = () => {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
   }
-}
+};
 
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-const generateHint = () =>  {
+const generateHint = (guess) => {
   // your code here
-}
+  // create variables solutionArray and guessArray that each split up passed in arguments
+  guess = guess.trim().toLowerCase();
+  let solutionArray = [];
+  solutionArray = solution.split("");
+  let guessArray = [];
+  guessArray = guess.split("");
+
+  let correctLetterLocation = 0;
+
+  for (let i = 0; i < solutionArray.length; i++) {
+    if (solutionArray[i] === guessArray[i]) {
+      correctLetterLocation++;
+      solutionArray[i] = null;
+    }
+  }
+
+  let correctLetter = 0;
+
+  for (let i = 0; i < solutionArray.length; i++) {
+    let targetIndex = solutionArray.indexOf(guessArray[i]);
+
+    if (targetIndex > -1) correctLetter++;
+    solutionArray[i] = null;
+  }
+
+  return `${correctLetterLocation}-${correctLetter}`;
+};
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
+  solution = "abcd"; // Comment this out to generate a random solution
   // your code here
-}
 
+  let hint = generateHint(guess);
 
-const getPrompt = () =>  {
-  rl.question('guess: ', (guess) => {
+  board.push(`${guess}, ${hint}`);
+  if (guess === solution) {
+    console.log("You guessed it!");
+  } else {
+    if (board.length > 10) {
+      console.log(`You ran out of turns! The solution was ${solution}`);
+      return `You ran out of turns! The solution was ${solution}`;
+    } else {
+      console.log("Guess again");
+      return "Guess again";
+    }
+  }
+
+  // if the guess you passed in equals the solution, return 'You guessed it
+};
+
+const getPrompt = () => {
+  rl.question("guess: ", (guess) => {
     mastermind(guess);
     printBoard();
     getPrompt();
   });
-}
+};
 
 // Tests
 
-if (typeof describe === 'function') {
-  solution = 'abcd';
-  describe('#mastermind()', () => {
-    it('should register a guess and generate hints', () => {
-      mastermind('aabb');
+if (typeof describe === "function") {
+  solution = "abcd";
+  describe("#mastermind()", () => {
+    it("should register a guess and generate hints", () => {
+      mastermind("aabb");
       assert.equal(board.length, 1);
     });
-    it('should be able to detect a win', () => {
-      assert.equal(mastermind(solution), 'You guessed it!');
+    it("should be able to detect a win", () => {
+      assert.equal(mastermind(solution), "You guessed it!");
     });
   });
 
-  describe('#generateHint()', () => {
-    it('should generate hints', () => {
-      assert.equal(generateHint('abdc'), '2-2');
+  describe("#generateHint()", () => {
+    it("should generate hints", () => {
+      assert.equal(generateHint("abdc"), "2-2");
     });
-    it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
+    it("should generate hints if solution has duplicates", () => {
+      assert.equal(generateHint("aabb"), "1-1");
     });
-
   });
-
 } else {
-
   generateSolution();
   getPrompt();
 }
